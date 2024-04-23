900080c7830b136c7593d1b9e91cb45179ac95fa
rishik puneet m <rishikpuneetm@gmail.com>
2024-04-23 21:47:40 +0530

    domain in the server cookie is set to off

--- src/controllers/login/login.controller.js
+++ src/controllers/login/login.controller.js
@@ -42,7 +42,7 @@ const login = async (req, res) => {
             path: "/",
             // sameSite: "strict",
             secure: true,
-            domain: ".onrender.com",
+            // domain: ".onrender.com",
             // domain: "localhost",
             // Other cookie options can be added here, like secure, sameSite, etc.
         });
